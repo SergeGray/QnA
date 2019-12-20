242964b85286030766e08015749c6a7d7a56fe47
Serge <s@shayderov.ru>
2019-12-20 19:20:23 +0300

    Add has_many association to Question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
+  it { should have_many(:answers) }
+
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
+
+  it "destroys dependent answers" do
+    question = FactoryBot.create(:question)
+    answer = FactoryBot.create(:answer)
+    question.answers << answer
+
+    expect { question.destroy }.to change { Answer.count }.by(-1)
+  end
 end
