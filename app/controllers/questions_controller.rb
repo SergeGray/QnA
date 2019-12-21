a3c471f9ef5c4217db28dc9a824e5c4b4cac618f
Serge <s@shayderov.ru>
2019-12-21 15:33:41 +0300

    QuestionsController#show action with specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,15 @@
 class QuestionsController < ApplicationController
+  before_action :set_question, only: :show
+
   def index
     @questions = Question.all
+  end
+
+  def show; end
+
+  private
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 3) }
 
@@ -12,6 +14,18 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders index view' do
       expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq(question)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template(:show)
     end
   end
 end
