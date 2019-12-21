6fb10b80903e8d7bc457d55d592a8b5214b7e84b
Serge <s@shayderov.ru>
2019-12-21 17:28:06 +0300

    AnswersController#edit action with specs

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,12 @@
 class AnswersController < ApplicationController
   before_action :set_question, only: :new
+  before_action :set_answer, only: :edit
 
   def new
     @answer = @question.answers.new
   end
+
+  def edit; end
 
   private
 
@@ -11,5 +14,7 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
   end
 
-
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
 end
