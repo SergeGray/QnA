4b80625bb66b9062dd53d23fc5f29b0d971e79a3
Serge <s@shayderov.ru>
2019-12-21 17:28:06 +0300

    AnswersController#edit action with specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { create(:question) }
+  let(:answer) { create(:answer) }
 
   describe 'GET #new' do
     before { get :new, params: { question_id: question.id } }
@@ -12,6 +13,18 @@ RSpec.describe AnswersController, type: :controller do
 
     it 'renders new view' do
       expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #edit' do
+    before { get :edit, params: { id: answer } }
+
+    it 'assigns the requested answer to @answer' do
+      expect(assigns(:answer)).to eq(answer)
+    end
+
+    it 'renders edit view' do
+      expect(response).to render_template(:edit)
     end
   end
 end
