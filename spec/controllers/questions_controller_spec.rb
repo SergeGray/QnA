6404fd5d47d31e63d32ebcccd71a2e2a5f930eed
Serge <s@shayderov.ru>
2019-12-21 15:59:01 +0300

    QuestionsController#create action with specs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -52,4 +52,34 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template(:edit)
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question to database' do
+        expect do
+          post :create, params: { question: attributes_for(:question) }
+        end.to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, params: { question: attributes_for(:question) }
+        expect(response).to redirect_to assigns(:question)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question' do
+        expect do
+          post :create, params: {
+            question: attributes_for(:question, :invalid)
+          }
+        end.to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :question do
     title { "How do I output stuff to console in Ruby?" }
     body { "I am new please help!" }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end
