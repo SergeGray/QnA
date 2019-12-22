5acbee34b38afaa7a8ee331fbd01c30f660d8a12
Serge <s@shayderov.ru>
2019-12-22 17:57:25 +0300

    Add sign out option

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'User can sign out', %q{
+  In order to close the session
+  As an authenticated user
+  I want to be able to sign out
+} do
+
+  given(:user) { create(:user) }
+
+  background { sign_in(user) }
+
+  scenario 'Authenticated user tries to sign out' do
+    click_link 'Sign out'
+
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
