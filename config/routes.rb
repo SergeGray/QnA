b7ea9a09e5a3e19213d6556932a07f6585290af6
Serge <s@shayderov.ru>
2019-12-22 13:12:43 +0300

    Add sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :questions do
     resources :answers, except: %i[index show], shallow: true
   end
+
+  root to: 'questions#index'
 end
