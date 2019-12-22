a80881febac830ea0cdd08767e420acbfb799835
Serge <s@shayderov.ru>
2019-12-22 17:10:44 +0300

    Add ability to view list of questions

---
+++ spec/features/question/list_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'User can view the list questions', %q{
+  In order to  find a question I am interested in
+  As any user
+  I want to be able to see the list of questions
+} do
+
+  given!(:question1) { create(:question) }
+  given!(:question2) { create(:question, :new) }
+
+  scenario 'Any user can view a list of all questions' do
+    visit questions_path
+    expect(page).to have_content question1.title
+    expect(page).to have_content question2.title
+  end
+end
