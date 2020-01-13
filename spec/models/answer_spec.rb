6003500f3114002fff09fd0fd4dce036599a9da4
Serge <s@shayderov.ru>
2020-01-13 18:29:03 +0300

    Persisted answers scope spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:user) }
 
   it { should validate_presence_of(:body) }
+
+  describe '.persisted' do
+    let(:question) { create(:question) }
+    let!(:answer1) { create(:answer, question: question) }
+
+    it 'returns persisted answers' do
+      question.answers.new(attributes_for(:answer, :new))
+
+      expect(question.answers.persisted).to contain_exactly(answer1)
+    end
+  end
 end
