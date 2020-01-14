e2312117cb21330b98e1beb20eb7fa02582a574b
Serge <s@shayderov.ru>
2020-01-14 14:08:04 +0300

    More specs refactoring

--- spec/features/question/view_spec.rb
+++ spec/features/question/view_spec.rb
@@ -6,13 +6,17 @@ feature 'User can view a question', %q(
   I want to be able to view a question with answers to it
 ) do
   given!(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question) }
+  given!(:answer1) { create(:answer, question: question) }
+  given!(:answer2) { create(:answer, :new, question: question) }
+  given!(:answer3) { create(:answer, body: 'This answer is not related') }
 
   scenario 'User tries to view a question' do
     visit questions_path
-    click_link 'Show'
+    click_link('Show', match: :first)
     expect(page).to have_content question.title
     expect(page).to have_content question.body
-    expect(page).to have_content answer.body
+    expect(page).to have_content answer1.body
+    expect(page).to have_content answer2.body
+    expect(page).to_not have_content answer3.body
   end
 end
