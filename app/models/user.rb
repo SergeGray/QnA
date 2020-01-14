275f904f5da0db08b0e617d2c4153b9ecf4e34ea
Serge <s@shayderov.ru>
2020-01-14 13:54:00 +0300

    Optimize User#author_of? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   def author_of?(resource)
-    resource.user == self
+    resource.user_id == self.id
   end
 end
