341fde87ef682ae02dda7e37889db58aed6faeb6
Serge <s@shayderov.ru>
2020-01-14 13:30:50 +0300

    Add user model method for checking resource ownership

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def check_ownership(attribute, path)
-    return if current_user == attribute.user
+    return if current_user.author_of?(attribute)
 
     redirect_to path,
                 alert: "You can't change other people's #{attribute.class}!"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password) }
+
+  describe '#owner_of?' do
+    let(:user) { create(:user) }
+
+    context 'when the user is owner of the resource' do
+      let!(:question) { create(:question, user: user) }
+
+      it 'returns true' do
+        expect(user.author_of?(question)).to be true
+      end
+    end
+
+    context 'when the user is not the owner of the resource' do
+      let(:question) { create(:question) }
+
+      it 'returns false' do
+        expect(user.author_of?(question)).to be false
+      end
+    end
+  end
 end
