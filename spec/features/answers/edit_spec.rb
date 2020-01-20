bc11dbb9565757cc2dee1ba5ffa373749217d5d3
Serge <s@shayderov.ru>
2020-01-20 12:18:44 +0300

    Additional answer edit specs

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -7,7 +7,7 @@ feature 'User can edit his answer', %q(
 ) do
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
-  given!(:answer) { create(:answer, question: question, user: user) }
+  given!(:answer) { create(:answer, question: question) }
 
   scenario 'Unauthenticated user tries to edit answer' do
     visit question_path(question)
@@ -18,23 +18,55 @@ feature 'User can edit his answer', %q(
   describe 'Authenticated user', js: true do
     background do
       sign_in(user)
-      visit question_path(question)
     end
 
-    scenario 'edits his answer' do
-      click_link 'Edit'
+    describe 'owns the answer' do
+      given!(:answer) { create(:answer, question: question, user: user) }
 
-      within '.answers' do
-        fill_in 'Body', with: 'edited answer'
-        click_button 'Save'
+      scenario 'tries to bring up the edit form' do
+        visit question_path(question)
 
-        expect(page).to_not have_content answer.body
-        expect(page).to have_content 'edited answer'
-        expect(page).to_not have_selector 'textarea'
+        within '.answers' do
+          expect(page).to_not have_selector 'textarea'
+
+          click_link 'Edit'
+
+          expect(page).to have_selector 'textarea'
+        end
+      end
+
+      background do
+        visit question_path(question)
+        click_link 'Edit'
+      end
+
+      scenario 'tries to edit it' do
+        within '.answers' do
+          fill_in 'Body', with: 'edited answer'
+          click_button 'Save'
+
+          expect(page).to_not have_content answer.body
+          expect(page).to have_content 'edited answer'
+          expect(page).to_not have_selector 'textarea'
+        end
+      end
+
+      scenario 'tries to edit it with errors' do
+        within '.answers' do
+          fill_in 'Body', with: ''
+          click_button 'Save'
+
+          expect(page).to have_selector 'textarea'
+        end
+
+        expect(page).to have_content "Body can't be blank"
       end
     end
 
-    scenario 'edits his answer with errors'
-    scenario "tries to edit someone else's answer"
+    scenario "tries to edit someone else's answer" do
+      visit question_path(question)
+
+      expect(page).to_not have_link 'Edit'
+    end
   end
 end
