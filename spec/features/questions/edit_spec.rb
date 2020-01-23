b6ca81cf774c44486d462eef8b1f93e32a717906
Serge <s@shayderov.ru>
2020-01-23 18:36:59 +0300

    Can add many attached files to questions and answers when editing

--- spec/features/questions/edit_spec.rb
+++ spec/features/questions/edit_spec.rb
@@ -63,6 +63,17 @@ feature 'User can edit his question', %q(
 
         expect(page).to have_content "Body can't be blank"
       end
+
+      scenario 'tries attach files to it' do
+        attach_file 'File', [
+          "#{Rails.root}/spec/rails_helper.rb",
+          "#{Rails.root}/spec/spec_helper.rb"
+        ]
+        click_button 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
     end
 
     scenario "tries to edit someone else's answer" do
