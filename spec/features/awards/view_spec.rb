14c237f245eb0fc794ae184f05721833ab9e7a5b
Serge <s@shayderov.ru>
2020-01-29 10:38:41 +0300

    Additional award display spec

--- spec/features/awards/view_spec.rb
+++ spec/features/awards/view_spec.rb
@@ -6,6 +6,7 @@ feature 'User can view his awards', %q(
   I want to be able to view my awards
 ) do
   given!(:user) { create(:user) }
+  given!(:user2) { create(:user2) }
   given!(:award) { create(:award, user: user) }
 
   scenario 'Authenticated user tries to see his awards' do
@@ -16,6 +17,16 @@ feature 'User can view his awards', %q(
     expect(page).to have_content award.title
     expect(page.find("#award-#{Award.last.id}")['src'])
       .to have_content award.image.filename.to_s
+  end
+
+  scenario "Authenticated user tries to see someone else's awards" do
+    sign_in(user2)
+
+    click_link 'View awards'
+
+    expect(page).to_not have_content award.title
+    expect(page.find("#award-#{Award.last.id}")['src'])
+      .to_not have_content award.image.filename.to_s 
   end
 
   scenario 'Guest tries to see awards' do
