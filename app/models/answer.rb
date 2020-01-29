60c864abbe075fca5dd48b586df47aa571a45f0e
Serge <s@shayderov.ru>
2020-01-29 11:32:55 +0300

    Move linkable logic to concerns

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,14 +1,12 @@
 class Answer < ApplicationRecord
+  include Linkable
+
   default_scope -> { order('best DESC, created_at') }
 
   belongs_to :question
   belongs_to :user
 
-  has_many :links, as: :linkable, dependent: :destroy
-
   has_many_attached :files
-
-  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
   validates :best, uniqueness: { scope: :question_id, best: true }, if: :best
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,14 +1,14 @@
 class Question < ApplicationRecord
+  include Linkable
+
   belongs_to :user
 
   has_many :answers, dependent: :destroy
-  has_many :links, as: :linkable, dependent: :destroy
 
   has_one :award, dependent: :destroy
 
   has_many_attached :files
 
-  accepts_nested_attributes_for :links, reject_if: :all_blank
   accepts_nested_attributes_for :award, reject_if: :all_blank
 
   validates :title, :body, presence: true
