409ea8899a585158278fa507b025c672234a5483
Serge <s@shayderov.ru>
2020-01-29 10:34:21 +0300

    Add shared linkable example

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
+require 'shared_model_examples'
 
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
-  it { should have_many(:links).dependent(:destroy) }
   it { should have_one(:award).dependent(:destroy) }
   it { should belong_to(:user) }
 
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:body) }
 
-  it { should accept_nested_attributes_for :links }
   it { should accept_nested_attributes_for :award }
+
+  it_behaves_like 'linkable'
 
   it 'has many attached files' do
     expect(Question.new.files)
