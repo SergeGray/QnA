465b1d539f4df92d8c923f49ee1ad653a34a3899
Serge <s@shayderov.ru>
2020-01-29 11:37:24 +0300

    Style fixes

--- spec/models/award_spec.rb
+++ spec/models/award_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe Award, type: :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :image }
 
-
   it 'has an attached image' do
     expect(Award.new.image)
       .to be_an_instance_of(ActiveStorage::Attached::One)
