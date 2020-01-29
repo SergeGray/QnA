f36eef0c4c5e67d337ae18549faebf69b52ae08c
Serge <s@shayderov.ru>
2020-01-29 10:33:40 +0300

    Add shared example for unauthorized actions

--- spec/controllers/attachments_controller_spec.rb
+++ spec/controllers/attachments_controller_spec.rb
@@ -1,41 +1,52 @@
 require 'rails_helper'
+require 'shared_controller_examples'
 
 RSpec.describe AttachmentsController, type: :controller do
   let(:user) { create(:user) }
   let(:question) { create(:question) }
 
   describe 'DELETE #destroy' do
-    before do
-      login(user)
-      question.files.attach(create_file_blob)
+
+    before { question.files.attach(create_file_blob) }
+
+    context 'when logged in' do
+      before { sign_in(user) }
+
+      context "on another user's question" do
+        it 'does not delete the attachment' do
+          expect do
+            delete :destroy, params: { id: question.files.first, format: :js }
+          end.to_not change(ActiveStorage::Attachment, :count)
+        end
+
+        it 'renders the destroy template' do
+          delete :destroy, params: { id: question.files.first, format: :js }
+          expect(response).to render_template(:destroy)
+        end
+      end
+
+      context "on user's own question" do
+        before { question.update!(user: user) }
+
+        it 'deletes the attachment' do
+          expect do
+            delete :destroy, params: { id: question.files.first, format: :js }
+          end.to change(question.files, :count).by(-1)
+        end
+
+        it 'renders the destroy template' do
+          delete :destroy, params: { id: question.files.first, format: :js }
+          expect(response).to render_template(:destroy)
+        end
+      end
     end
 
-    context "on another user's question" do
-      it 'does not delete the attachment' do
-        expect do
-          delete :destroy, params: { id: question.files.first, format: :js }
-        end.to_not change(ActiveStorage::Attachment, :count)
+    context 'when not logged in' do
+      before do
+        delete :destroy, params: { id: question.files.first, format: :js } 
       end
 
-      it 'renders the destroy template' do
-        delete :destroy, params: { id: question.files.first, format: :js }
-        expect(response).to render_template(:destroy)
-      end
-    end
-
-    context "on user's own question" do
-      before { question.update!(user: user) }
-
-      it 'deletes the attachment' do
-        expect do
-          delete :destroy, params: { id: question.files.first, format: :js }
-        end.to change(question.files, :count).by(-1)
-      end
-
-      it 'renders the destroy template' do
-        delete :destroy, params: { id: question.files.first, format: :js }
-        expect(response).to render_template(:destroy)
-      end
+      it_behaves_like 'malicious action'
     end
   end
 end
--- spec/controllers/awards_controller_spec.rb
+++ spec/controllers/awards_controller_spec.rb
@@ -1,20 +1,30 @@
 require 'rails_helper'
+require 'shared_controller_examples'
 
 RSpec.describe AwardsController, type: :controller do
   let(:user) { create(:user) }
   let(:award) { create(:award, user: user) }
 
-  before { login(user) }
-
   describe 'GET #index' do
-    before { get :index }
+    context 'when logged in' do
+      before do
+        login(user)
+        get :index
+      end
 
-    it "assigns @awards to be a collection of current user's awards" do
-      expect(assigns(:awards)).to match_array(user.awards)
+      it "assigns @awards to be a collection of current user's awards" do
+        expect(assigns(:awards)).to match_array(user.awards)
+      end
+
+      it 'renders index template' do
+        expect(response).to render_template :index
+      end
     end
 
-    it 'renders index template' do
-      expect(response).to render_template :index
+    context 'when not logged in' do
+      before { get :index }
+
+      it_behaves_like 'unauthorized action'
     end
   end
 end
