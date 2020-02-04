ae2b67a87103c00a1037ca157e94e2b920aa7750
Serge <s@shayderov.ru>
2020-02-04 09:35:16 +0300

    Additional links controller specs

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared/controller_examples'
 
 RSpec.describe LinksController, type: :controller do
   let(:user) { create(:user) }
@@ -6,33 +7,49 @@ RSpec.describe LinksController, type: :controller do
   let!(:link) { create(:link, linkable: question) }
 
   describe 'DELETE #destroy' do
-    before { login(user) }
+    describe 'Authenticated user' do
+      before { login(user) }
 
-    context "on another user's question" do
+      context "on another user's question" do
+        it 'does not delete the attachment' do
+          expect do
+            delete :destroy, params: { id: link, format: :js }
+          end.to_not change(Link, :count)
+        end
+
+        it 'renders the destroy template' do
+          delete :destroy, params: { id: link, format: :js }
+          expect(response).to render_template(:destroy)
+        end
+      end
+
+      context "on user's own question" do
+        before { question.update!(user: user) }
+
+        it 'deletes the attachment' do
+          expect do
+            delete :destroy, params: { id: link, format: :js }
+          end.to change(question.links, :count).by(-1)
+        end
+
+        it 'renders the destroy template' do
+          delete :destroy, params: { id: link, format: :js }
+          expect(response).to render_template(:destroy)
+        end
+      end
+    end
+
+    describe 'Unauthenticated user' do
       it 'does not delete the attachment' do
         expect do
           delete :destroy, params: { id: link, format: :js }
         end.to_not change(Link, :count)
       end
 
-      it 'renders the destroy template' do
-        delete :destroy, params: { id: link, format: :js }
-        expect(response).to render_template(:destroy)
-      end
-    end
+      context 'after action is called' do
+        before { delete :destroy, params: { id: link, format: :js } }
 
-    context "on user's own question" do
-      before { question.update!(user: user) }
-
-      it 'deletes the attachment' do
-        expect do
-          delete :destroy, params: { id: link, format: :js }
-        end.to change(question.links, :count).by(-1)
-      end
-
-      it 'renders the destroy template' do
-        delete :destroy, params: { id: link, format: :js }
-        expect(response).to render_template(:destroy)
+        it_behaves_like 'malicious action'
       end
     end
   end
