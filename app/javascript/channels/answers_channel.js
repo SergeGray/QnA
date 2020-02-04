517e45af4722f8782950eaa658c886213ff39931
Serge <s@shayderov.ru>
2020-02-04 11:02:08 +0300

    Add stream for answer creation

--- app/javascript/channels/answers_channel.js
+++ app/javascript/channels/answers_channel.js
@@ -1,13 +1,12 @@
-import consumer from "./consumer"
+import consumer from './consumer'
 
 consumer.subscriptions.create(
   { 
-    channel: "AnswersChannel",
+    channel: 'AnswersChannel',
     question_id: gon.question_id
   }, {
     received(data) {
-      let answersList = $(".answers")
-      answersList.append(data)
+      $('.answers').append(data)
     }
   }
 )
--- spec/features/comments/create_spec.rb
+++ spec/features/comments/create_spec.rb
@@ -64,6 +64,61 @@ feature 'User can create a comment', %q(
     end
   end
 
+  context 'In multiple sessions', js: true do
+    scenario "new question comment appears on another user's page" do
+      using_session('guest') do
+        visit question_path(question)
+
+        expect(page).to_not have_content 'Good question'
+      end
+
+      using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+        
+        within(".question-#{question.id}-comments") do
+          click_link 'New comment'
+
+          fill_in 'comment[body]', with: 'Good question'
+          click_button 'Comment'
+        end
+      end
+
+      using_session('guest') do
+        within(".question-#{question.id}-comments") do
+          expect(page).to have_content 'Good question'
+        end
+      end
+    end
+
+    scenario "new answer comment appears on another user's page" do
+      using_session('guest') do
+        visit question_path(question)
+        
+        expect(page).to_not have_content 'Good answer'
+      end
+
+      using_session('user') do
+        sign_in(user)
+        visit question_path(question)
+        
+        within(".answer-#{answer.id}-comments") do
+          click_link 'New comment'
+
+          fill_in 'comment[body]', with: 'Good answer'
+          click_button 'Comment'
+        end
+      end
+
+      using_session('guest') do
+
+        within(".answer-#{answer.id}-comments") do
+          expect(page).to have_content 'Good answer'
+        end
+      end
+    end
+  end
+
   scenario 'Unauthenticated user tries to create a question comment' do
     visit question_path(question)
 
