7982d34ed6ef272aa0016988c5ee96b452986763
Serge <s@shayderov.ru>
2020-02-04 09:35:05 +0300

    Additional questions controller specs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'shared/controller_examples'
 
 RSpec.describe QuestionsController, type: :controller do
   it_behaves_like VotableActions do
@@ -35,80 +36,195 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
-    before { login(user) }
+    describe 'Authenticated user' do
+      before do
+        login(user)
+        get :new
+      end
 
-    before { get :new }
+      it 'assigns a new question to @question' do
+        expect(assigns(:question)).to be_a_new(Question)
+      end
 
-    it 'assigns a new question to @question' do
-      expect(assigns(:question)).to be_a_new(Question)
+      it 'assigns a new award to @question.award' do
+        expect(assigns(:question).award).to be_a_new(Award)
+      end
+
+      it 'renders new view' do
+        expect(response).to render_template(:new)
+      end
     end
 
-    it 'assigns a new award to @question.award' do
-      expect(assigns(:question).award).to be_a_new(Award)
-    end
+    describe 'Unauthenticated user' do
+      before { get :new }
 
-    it 'renders new view' do
-      expect(response).to render_template(:new)
+      it 'does not assign a new question to @ question' do
+        expect(assigns(:question)).to_not be_a_new(Question)
+      end
+
+      it 'does not assign a new award to @question.award' do
+        expect(assigns(:question)).to_not respond_to(:award)
+      end
+
+      it_behaves_like 'unauthorized action'
     end
   end
 
   describe 'POST #create' do
-    before { login(user) }
+    describe 'Authenticated user' do
+      before { login(user) }
 
-    context 'with valid attributes' do
-      it 'saves a new question to database' do
-        expect do
+      context 'with valid attributes' do
+        it 'saves a new question to database' do
+          expect do
+            post :create, params: { question: attributes_for(:question) }
+          end.to change(Question, :count).by(1)
+        end
+
+        it 'redirects to show view' do
           post :create, params: { question: attributes_for(:question) }
-        end.to change(Question, :count).by(1)
+          expect(response).to redirect_to assigns(:question)
+        end
+
+        it 'broadcasts the new question' do
+          expect do
+            post :create, params: {
+              question: attributes_for(:question),
+              format: :js
+            }
+          end.to have_broadcasted_to('questions')
+        end
       end
 
-      it 'redirects to show view' do
-        post :create, params: { question: attributes_for(:question) }
-        expect(response).to redirect_to assigns(:question)
+      context 'with invalid attributes' do
+        it 'does not save the question' do
+          expect do
+            post :create, params: {
+              question: attributes_for(:question, :invalid)
+            }
+          end.to_not change(Question, :count)
+        end
+
+        it 're-renders new view' do
+          post :create, params: { question: attributes_for(:question, :invalid) }
+          expect(response).to render_template(:new)
+        end
+
+        it 'does not broadcast the new question' do
+          expect do
+            post :create, params: {
+              question: attributes_for(:question, :invalid),
+              format: :js
+            }
+          end.to_not have_broadcasted_to('questions')
+        end
       end
     end
 
-    context 'with invalid attributes' do
+    describe 'Unauthenticated user' do
       it 'does not save the question' do
         expect do
           post :create, params: {
-            question: attributes_for(:question, :invalid)
+            question: attributes_for(:question, :invalid),
+            format: :js
           }
         end.to_not change(Question, :count)
       end
 
-      it 're-renders new view' do
-        post :create, params: { question: attributes_for(:question, :invalid) }
-        expect(response).to render_template(:new)
+      context 'after action is called' do
+        before do
+          post :create, params: {
+            question: attributes_for(:question, :invalid),
+            format: :js
+          }
+        end
+
+        it_behaves_like 'malicious action'
+      end
+
+      it 'does not broadcast the new question' do
+        expect do
+          post :create, params: {
+            question: attributes_for(:question, :invalid),
+            format: :js
+          }
+        end.to_not have_broadcasted_to('questions')
       end
     end
   end
 
   describe 'PATCH #update' do
-    before { login(user) }
+    describe 'Authenticated user' do
+      before { login(user) }
 
-    context "on another user's question" do
-      let(:user2) { create(:user) }
-      let(:question2) { create(:question, user: user2) }
+      context "on another user's question" do
+        let(:user2) { create(:user) }
+        let(:question2) { create(:question, user: user2) }
 
-      before do
-        patch :update, params: {
-          id: question2,
-          question: attributes_for(:question, :new),
-          format: :js
-        }
+        before do
+          patch :update, params: {
+            id: question2,
+            question: attributes_for(:question, :new),
+            format: :js
+          }
+        end
+
+        it 'does not change the question' do
+          expect { question2.reload }.to_not change(question2, :attributes)
+        end
+
+        it 'redirects to index view' do
+          expect(response).to redirect_to(questions_path)
+        end
       end
 
-      it 'does not change the question' do
-        expect { question2.reload }.to_not change(question2, :attributes)
+      context 'with valid attributes' do
+        before do
+          patch :update, params: {
+            id: question,
+            question: attributes_for(:question, :new),
+            format: :js
+          }
+        end
+
+        it 'assigns the requested question to @question' do
+          expect(assigns(:question)).to eq(question)
+        end
+
+        it 'changes question attributes' do
+          expect { question.reload }
+            .to change(question, :title).to(
+              attributes_for(:question, :new)[:title]
+            ).and change(question, :body).to(
+              attributes_for(:question, :new)[:body]
+            )
+        end
+
+        it 'renders the update template' do
+          expect(response).to render_template(:update)
+        end
       end
 
-      it 'redirects to index view' do
-        expect(response).to redirect_to(questions_path)
+      context 'with invalid attributes' do
+        before do
+          patch :update, params: {
+            id: question,
+            question: attributes_for(:question, :invalid),
+            format: :js
+          }
+        end
+
+        it 'does not change the question' do
+          expect { question.reload }.to_not change(question, :attributes)
+        end
+
+        it 'renders the update template' do
+          expect(response).to render_template(:update)
+        end
       end
     end
 
-    context 'with valid attributes' do
+    describe 'Unauthenticated user' do
       before do
         patch :update, params: {
           id: question,
@@ -117,74 +233,65 @@ RSpec.describe QuestionsController, type: :controller do
         }
       end
 
-      it 'assigns the requested question to @question' do
-        expect(assigns(:question)).to eq(question)
-      end
-
-      it 'changes question attributes' do
-        expect { question.reload }
-          .to change(question, :title).to(
-            attributes_for(:question, :new)[:title]
-          ).and change(question, :body).to(
-            attributes_for(:question, :new)[:body]
-          )
-      end
-
-      it 'renders the update template' do
-        expect(response).to render_template(:update)
-      end
-    end
-
-    context 'with invalid attributes' do
-      before do
-        patch :update, params: {
-          id: question,
-          question: attributes_for(:question, :invalid),
-          format: :js
-        }
-      end
-
-      it 'does not change the question' do
+      it 'does not save the question' do
         expect { question.reload }.to_not change(question, :attributes)
       end
 
-      it 'renders the update template' do
-        expect(response).to render_template(:update)
-      end
+      it_behaves_like 'malicious action'
     end
   end
 
   describe 'DELETE #destroy' do
-    before { login(user) }
+    describe 'Authenticated user' do
+      before { login(user) }
 
-    context "on another user's question" do
-      let(:user2) { create(:user) }
-      let!(:question2) { create(:question, user: user2) }
+      context "on another user's question" do
+        let(:user2) { create(:user) }
+        let!(:question2) { create(:question, user: user2) }
 
-      it 'does not delete the question' do
-        expect do
+        it 'does not delete the question' do
+          expect do
+            delete :destroy, params: { id: question2 }
+          end.to_not change(Question, :count)
+        end
+
+        it 'redirects to index view' do
           delete :destroy, params: { id: question2 }
-        end.to_not change(Question, :count)
+          expect(response).to redirect_to(questions_path)
+        end
       end
 
-      it 'redirects to index view' do
-        delete :destroy, params: { id: question2 }
-        expect(response).to redirect_to(questions_path)
+      context "on user's own question" do
+        let!(:question) { create(:question, user: user) }
+
+        it 'deletes the question' do
+          expect do
+            delete :destroy, params: { id: question }
+          end.to change(Question, :count).by(-1)
+        end
+
+        it 'redirects to index view' do
+          delete :destroy, params: { id: question }
+          expect(response).to redirect_to questions_path
+        end
       end
     end
 
-    context "on user's own question" do
-      let!(:question) { create(:question, user: user) }
+    describe 'Unauthenticated user' do
+      let!(:question) { create(:question) }
 
-      it 'deletes the question' do
+      it 'does not delete the question' do
         expect do
-          delete :destroy, params: { id: question }
-        end.to change(Question, :count).by(-1)
+          delete :destroy, params: { id: question, format: :js }
+        end.to_not change(Question, :count)
       end
 
-      it 'redirects to index view' do
-        delete :destroy, params: { id: question }
-        expect(response).to redirect_to questions_path
+      context 'after action is called' do
+        before do
+          delete :destroy, params: { id: question, format: :js }
+        end
+
+        it_behaves_like 'malicious action'
       end
     end
   end
