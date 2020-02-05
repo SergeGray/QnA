d4e4cab028d79e37c99162fd3039e66c56ce7465
Serge <s@shayderov.ru>
2020-02-05 16:49:55 +0300

    Oauth callbacks controller specs

--- app/controllers/oauth_callbacks_controller.rb
+++ app/controllers/oauth_callbacks_controller.rb
@@ -1,5 +1,14 @@
 class OauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    render json: request.env['omniauth.auth']
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+
+    if @user&.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      if is_navigational_format?
+        set_flash_message(:notice, :success, kind: 'Github')
+      end
+    else
+      redirect_to root_path, alert: 'Something went wrong'
+    end
   end
 end
