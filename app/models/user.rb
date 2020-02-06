29e694668490cb480b1b24879e594ba2316e64f1
Serge <s@shayderov.ru>
2020-02-06 13:04:39 +0300

    Realize User.find_for_oauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,38 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :authorizations, dependent: :destroy
   has_many :awards
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
+  def self.find_for_oauth(auth)
+    authorization = Authorization.find_by(auth_params(auth))
+    return authorization.user if authorization
+
+    user = find_or_create(auth.info[:email])
+    user.authorizations.create(auth_params(auth))
+    user
+  end
+
   def author_of?(resource)
     resource.user_id == id
   end
 
-  def self.find_for_oauth(auth); end
+  private
+
+  def self.find_or_create(email)
+    password = Devise.friendly_token[0, 20]
+    User.find_by(email: email) || User.create!(
+      email: email,
+      password: password,
+      password_confirmation: password
+    )
+  end
+
+  def self.auth_params(auth)
+    { provider: auth.provider, uid: auth.uid.to_s }
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_03_144342) do
+ActiveRecord::Schema.define(version: 2020_02_06_075059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2020_02_03_144342) do
     t.index ["question_id", "best"], name: "index_answers_on_question_id_and_best", unique: true, where: "(best IS TRUE)"
     t.index ["question_id"], name: "index_answers_on_question_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "authorizations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_authorizations_on_user_id"
   end
 
   create_table "awards", force: :cascade do |t|
@@ -114,6 +123,7 @@ ActiveRecord::Schema.define(version: 2020_02_03_144342) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
   add_foreign_key "answers", "users"
+  add_foreign_key "authorizations", "users"
   add_foreign_key "awards", "questions"
   add_foreign_key "awards", "users"
   add_foreign_key "comments", "users"
