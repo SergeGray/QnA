0b6ae8bf833a2c57499468b869a83ce2b7add76d
Serge <s@shayderov.ru>
2020-02-06 13:06:02 +0300

    Slight spec style changes

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Question, type: :model do
   it { should have_many(:answers).dependent(:destroy) }
   it { should have_one(:award).dependent(:destroy) }
-  it { should belong_to(:user) }
+  it { should belong_to :user }
 
-  it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:body) }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
 
   it { should accept_nested_attributes_for :award }
 
