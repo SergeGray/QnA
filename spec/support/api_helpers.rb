ea54da9f55bbab9545d9aa7103113fdf5957a90f
Serge <s@shayderov.ru>
2020-02-18 18:38:26 +0300

    Shared examplpes for api specs

--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -2,4 +2,12 @@ module ApiHelpers
   def json
     @json ||= JSON.parse(response.body)
   end
+
+  def headers
+    { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+  end
+
+  def do_request(method, path, options = {})
+    send method, path, options
+  end
 end
