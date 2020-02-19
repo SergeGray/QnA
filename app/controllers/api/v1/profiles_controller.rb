d6398e4a7992703e942e3be2ba0f9c8bef4b348b
Serge <s@shayderov.ru>
2020-02-19 15:26:09 +0300

    Add get all but current users api

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ProfilesController < Api::V1::BaseController
   def me
     render json: current_resource_owner
   end
+
+  def index
+    render json: User.where('id <> ?', current_resource_owner.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :profiles, only: [] do
+      resources :profiles, only: :index do
         get :me, on: :collection
       end
 
