bd0e1fbfada485841643c2d77f9f8a1d85b022b4
Serge <s@shayderov.ru>
2020-02-25 17:17:31 +0300

    Add get answer api

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::AnswersController < Api::V1::BaseController
     @question = Question.find(params[:question_id])
     render json: @question.answers
   end
+
+  def show
+    render json: Answer.find(params[:id])
+  end
 end
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -28,4 +28,80 @@ describe 'Answerss API', type: :request do
       end
     end
   end
+  
+  describe 'GET /api/v1/answers/:id' do
+    let!(:answer) { create(:answer) }
+    let(:answer_path) { "#{api_path}/answers/#{answer.id}" }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :get }
+      let(:path) { answer_path }
+    end
+
+    context 'authorized' do
+      let(:access_token) { create(:access_token) }
+      let(:answer_response) { json['answer'] }
+
+      it_behaves_like 'API get one' do
+        let(:resource) { answer }
+        let(:resource_response) { answer_response }
+
+        before do
+          get answer_path, 
+              params: { access_token: access_token.token },
+              headers: headers
+        end
+      end
+
+      context 'with comments' do
+        let!(:comments) { create_list(:comment, 3, commentable: answer) }
+
+        before do
+          get answer_path, 
+              params: { access_token: access_token.token },
+              headers: headers
+        end
+
+        it_behaves_like 'API get many' do
+          let!(:resource_list) { comments }
+          let(:resource_json) { answer_response['comments'] }
+          let(:public_fields) { %w[id body user_id created_at updated_at] }
+        end
+      end
+
+      context 'with attached files' do
+        let(:resource_list) { answer.files }
+        let(:resource_json) { answer_response['files'] }
+        let(:public_fields) { %w[id filename] }
+
+        before do
+          3.times { answer.files.attach(create_file_blob) }
+
+          get answer_path, 
+              params: { access_token: access_token.token },
+              headers: headers
+        end
+
+        it_behaves_like 'API get many'
+        
+        it_behaves_like 'API get file url'
+      end
+
+      context 'with attached links' do
+        let!(:links) { create_list(:link, 3, linkable: answer) }
+
+        before do
+          get answer_path, 
+              params: { access_token: access_token.token },
+              headers: headers
+        end
+
+        it_behaves_like 'API get many' do
+          let!(:resource_list) { links }
+          let(:resource_json) { json['answer']['links'] }
+          let(:public_fields) { %w[id name url created_at updated_at] }
+        end
+      end
+    end
+  end
 end
