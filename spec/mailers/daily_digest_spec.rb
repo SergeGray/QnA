5a61461b034c34fbcbdecfffc5bd315f4b249e94
Serge <s@shayderov.ru>
2020-03-02 17:50:42 +0300

    Mailer specs

---
+++ spec/mailers/daily_digest_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe DailyDigestMailer, type: :mailer do
+  describe '#digest' do
+    let(:mail) { DailyDigestMailer.digest(user) }
+    let(:user) { create(:user) }
+    let(:new_questions) { create_list(:question, 2) }
+    let(:old_questions) do
+      create_list(:question, 2, created_at: Time.zone.now - 2.days)
+    end
+
+    it 'emails new questions' do
+      new_questions.each do |question|
+        expect(mail.body.encoded).to have_content(question.title)
+      end
+    end
+
+    it 'does not email old questions' do
+      old_questions.each do |question|
+        expect(mail.body.encoded).to_not have_content(question.title)
+      end
+    end
+  end
+end
