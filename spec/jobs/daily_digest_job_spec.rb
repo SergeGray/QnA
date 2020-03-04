05b2e51335c619efda2c13422006433e7cced43c
Serge <s@shayderov.ru>
2020-03-04 12:25:02 +0300

    Remove unneeded job

--- spec/jobs/daily_digest_job_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe DailyDigestJob, type: :job do
-  it 'calls DailyDigestService#call' do
-    expect(DailyDigestService).to receive(:call)
-    DailyDigestJob.perform_now
-  end
-end
