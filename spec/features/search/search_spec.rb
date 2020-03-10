54746c464ad948b836592e40ebf496be78699915
Serge <s@shayderov.ru>
2020-03-10 10:28:48 +0300

    Add question search

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -4,7 +4,7 @@ feature 'User can search resources', %q(
   In order to find existing information on my problem
   As any user
   I want to be able to search resources
-) do
+), sphinx: true, js: true do
   given!(:ruby_question) { create(:question, title: 'How do I install Ruby?') }
   given!(:other_question) { create(:question, title: 'Help me!') }
   given!(:ruby_answer) { create(:answer, body: 'Use rvm to install ruby') }
@@ -14,21 +14,57 @@ feature 'User can search resources', %q(
   given!(:ruby_user) { create(:user, email: 'ruby.user@gmail.com') }
   given!(:other_user) { create(:user) }
 
-  scenario 'User tries to do a global search', sphinx: true, js: true do
-    visit root_path
+  given!(:ruby_attributes) do
+    [
+      ruby_question.title,
+      ruby_answer.body,
+      ruby_comment.body,
+      ruby_user.email
+    ]
+  end
 
+  given!(:other_attributes) do
+    [
+      other_question.title,
+      other_answer.body,
+      other_comment.body,
+      other_user.email
+    ]
+  end
+
+
+  before do
+    visit root_path
+    fill_in 'query', with: 'ruby'
+  end
+
+  after do   
+    other_attributes.each do |attribute|
+      expect(page).to_not have_content attribute
+    end
+  end
+
+
+  scenario 'User tries to do a global search' do
     ThinkingSphinx::Test.run do
-      fill_in 'query', with: 'ruby'
+      click_button 'Search'
+      
+      ruby_attributes.each do |attribute|
+        expect(page).to have_content attribute
+      end
+    end
+  end
+
+  scenario 'User tries to do a question search' do
+    ThinkingSphinx::Test.run do
+      select 'Question', from: 'resource'
       click_button 'Search'
 
       expect(page).to have_content ruby_question.title
-      expect(page).to have_content ruby_answer.body
-      expect(page).to have_content ruby_comment.body
-      expect(page).to have_content ruby_user.email
-      expect(page).to_not have_content other_question.title
-      expect(page).to_not have_content other_answer.body
-      expect(page).to_not have_content other_comment.body
-      expect(page).to_not have_content other_user.email
+
+      (ruby_attributes - [ruby_question.title]).each do |attribute|
+        expect(page).to_not have_content attribute
+      end
     end
   end
 end
