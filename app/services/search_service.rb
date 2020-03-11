9dc271437423a0a5e31b708aa53a7eb7e9f34f80
Serge <s@shayderov.ru>
2020-03-11 12:04:26 +0300

    Search service refactoring

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,14 +1,9 @@
 class SearchService
   RESOURCES = %w[Question Answer Comment User].freeze
 
-  def self.call(query: '', resource: nil)
-    klass = if RESOURCES.include?(resource)
-              resource.constantize
-            else
-              ThinkingSphinx
-            end
-
-    escape_query = ThinkingSphinx::Query.escape(query)
-    klass.search escape_query
+  def self.call(query: , resource: nil)
+    klass = RESOURCES.include?(resource) ? resource.constantize : ThinkingSphinx
+    escaped_query = ThinkingSphinx::Query.escape(query)
+    klass.search escaped_query
   end
 end
